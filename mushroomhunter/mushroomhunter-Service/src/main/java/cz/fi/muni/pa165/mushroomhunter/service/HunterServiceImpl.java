dc6284de47829dd1d39bd0738b4e212299a20b7f
lapwings <r.cejka@seznam.cz>
2014-11-07 14:42:17 +0100

    change HunterServisImpl
    Add commnets, trasaction

--- mushroomhunter/mushroomhunter-Service/src/main/java/cz/fi/muni/pa165/mushroomhunter/service/HunterServiceImpl.java
+++ mushroomhunter/mushroomhunter-Service/src/main/java/cz/fi/muni/pa165/mushroomhunter/service/HunterServiceImpl.java
@@ -11,10 +11,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  *
- * @author RaD
+ * @author Radim Cejka
  */
 @Service
 public class HunterServiceImpl implements HunterService{
@@ -24,6 +25,13 @@ public class HunterServiceImpl implements HunterService{
     @Autowired
     private HunterConverter hunterConverter;
     
+      /**
+    * Saves hunter into database.
+    * 
+    * @param hunter The hunter to be saved.
+    * @return ID of the saved hunter.
+    */
+    @Transactional
     @Override
     public long save(HunterDto hunterDto) {
         if (hunterDto == null) {
@@ -38,6 +46,13 @@ public class HunterServiceImpl implements HunterService{
         
     }
 
+     /**
+    * Updates given hunter in database.
+    * 
+    * @param hunter The hunter to be updated.
+    * @return Updated hunter.
+    */
+    @Transactional
     @Override
     public HunterDto update(HunterDto hunterDto) {
          if (hunterDto == null) {
@@ -50,7 +65,12 @@ public class HunterServiceImpl implements HunterService{
             throw new DataRetrievalFailureException("Error updating data.", e);
         }
     }
-
+/**
+     * Deletes given hunter from the database.
+     * 
+     * @param hunter The hunter to be deleted.
+     */
+    @Transactional
     @Override
     public void delete(HunterDto hunterDto) {
          if (hunterDto == null) {
@@ -62,7 +82,14 @@ public class HunterServiceImpl implements HunterService{
             throw new DataRetrievalFailureException("Error deleting data.", e);
         }    
     }
-
+    
+/**
+     * Finds a hunter by ID.
+     * 
+     * @param id The ID of the searched hunter.
+     * @return The found hunter.
+     */
+    @Transactional
     @Override
     public HunterDto find(long id) {
         try {
@@ -72,6 +99,13 @@ public class HunterServiceImpl implements HunterService{
         }
     }
 
+    /**
+     * Finds all hunters with given first name.
+     * 
+     * @param surname First name of the hunter.
+     * @return The list of all hunters with with given first name.
+     */
+    @Transactional
     @Override
     public List<HunterDto> findByName(String firstName) {
          try {
@@ -81,6 +115,13 @@ public class HunterServiceImpl implements HunterService{
         }
     }
 
+    /**
+     * Finds all hunters with given surname.
+     * 
+     * @param surname Surname of the hunter.
+     * @return The list of all hunters with with given surname.
+     */
+    @Transactional
     @Override
     public List<HunterDto> findBySurname(String surname) {
          try {
@@ -90,6 +131,13 @@ public class HunterServiceImpl implements HunterService{
         }
     }
 
+    /**
+     * Finds all hunters with the given nick name.
+     * 
+     * @param nick The nick name of the hunter.
+     * @return The list of all hunters with with the given nick name.
+     */
+    @Transactional
     @Override
     public List<HunterDto> findByNick(String nick) {
          try {
@@ -99,6 +147,12 @@ public class HunterServiceImpl implements HunterService{
         }
     }
 
+    /**
+     * Finds all hunters in the database.
+     * 
+     * @return The list of all hunters.
+     */
+    @Transactional
     @Override
     public List<HunterDto> findAll() {
          try {
